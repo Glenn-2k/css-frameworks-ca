c95557aa8dd52040cbbc256397d68f85739397b2
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-04-10 23:41:30 +0200

    started on login

---
+++ js/handlers/login.mjs
@@ -0,0 +1,17 @@
+import { login } from "../api/auth/login.mjs";
+
+export function setLoginFormListener() {
+    const form = document.querySelector('#loginForm');
+    
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const form = event.target
+        const formData = new FormData(form);
+        const profile = Object.fromEntries(formData.entries())
+        console.log(profile);
+        
+        // sending the profile object to the login function
+        login(profile);
+
+    });
+};
--- js/index.mjs
+++ js/index.mjs
@@ -1,3 +1,11 @@
 import { setRegisterFormListener } from "./handlers/register.mjs";
+import { setLoginFormListener } from "./handlers/login.mjs";
 
-setRegisterFormListener();
+
+const path = location.pathname;
+
+if (path === "/index.html") {
+    setLoginFormListener();
+} else if (path === "/register/index.html") {
+    setRegisterFormListener();
+}
