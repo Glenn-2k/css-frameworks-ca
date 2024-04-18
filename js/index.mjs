7d4b720fda8553cb9cc619984d0ca66cc912c1bf
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-04-18 13:32:14 +0200

    fixed logout function

--- js/index.mjs
+++ js/index.mjs
@@ -3,7 +3,7 @@ import { setLoginFormListener } from "./handlers/login.mjs";
 import { runPosts } from "./templates/post.mjs";
 import * as post from "./api/posts/index.mjs";
 import * as templates from "./templates/index.mjs";
-import { logOutFunction } from "./handlers/logout.mjs";
+import { logOutFunction } from "./api/utilities/logOut.mjs";
 
 const path = location.pathname;
 
@@ -15,16 +15,18 @@ if (path === "/" || path === "/index.html") {
 } else if (path === "/feed/index.html" || path === "/feed/") {
     runPosts();
     console.log("Feed page is initialized.");
+    logOutFunction();
 }  else if (path === "/profile/index.html" || path === "/profile/") {
     console.log("Profile page is initialized.");
     window.confirm("You are now on the profile page.");
+    logOutFunction();
 }
 
 
 
 // functions I want to run on all pages // 
 
-logOutFunction();
+
 
 
 
