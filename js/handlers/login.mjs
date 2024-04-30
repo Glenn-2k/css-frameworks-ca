b597643b9fb3d6406d0546ff544e54bb8a2f82ba
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-04-30 10:28:37 +0200

    login function working

--- js/handlers/login.mjs
+++ js/handlers/login.mjs
@@ -1,17 +1,33 @@
 import { login } from '../api/auth/login.mjs';
+import * as storage from '../storage/index.mjs';
 
 export function setLoginFormListener() {
   const form = document.querySelector('#loginForm');
 
-  form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const formData = new FormData(form);
-    const profile = Object.fromEntries(formData.entries());
-    console.log(profile);
+  if (form) {
+    form.addEventListener('submit', async (event) => {
+      event.preventDefault();
+      const form = event.target;
+      const formData = new FormData(form);
+      const profile = Object.fromEntries(formData.entries());
+      console.log(profile);
 
-    // sending the profile object to the login function
-    login(profile);
-    // window.location.href = '/profile';
-  });
+      try {
+        await login(profile);
+        const user = storage.load('profile');
+
+        if (user && user.name) {
+          window.location.href = `/profile/?name=${user.name}`;
+        } else {
+          throw new Error('Invalid user');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }
+
+  // sending the profile object to the login function
+  // login(profile);
+  // window.location.href = '/profile';
 }
