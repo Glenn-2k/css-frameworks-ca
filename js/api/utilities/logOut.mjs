152b8be34fa7f96fbd74d332137ea5e2fb8f45fb
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-05-10 14:34:53 +0200

    added some JSDocs

--- js/api/utilities/logOut.mjs
+++ js/api/utilities/logOut.mjs
@@ -1,3 +1,12 @@
+/**
+ * Attaches an event listener to a logout button that clears the local storage and redirects the user to the homepage.
+ *
+ * @example
+ * // Assuming there is a button with id 'logOutButton' in your HTML
+ * logOutFunction();
+ * // Clicking the 'logOutButton' will clear the local storage and redirect to the homepage ('/').
+ */
+
 export function logOutFunction() {
   const logOutButton = document.getElementById('logOutButton');
 
