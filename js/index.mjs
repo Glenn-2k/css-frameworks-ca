2c9f40e65723e0b79ea1024a850e36f4a9616f94
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-05-16 11:50:19 +0200

    removed unused imports

--- js/index.mjs
+++ js/index.mjs
@@ -1,15 +1,11 @@
 import { setRegisterFormListener } from './handlers/register.mjs';
 import { setLoginFormListener } from './handlers/login.mjs';
 import { runPosts } from './templates/post.mjs';
-import * as post from './api/posts/index.mjs';
-import * as templates from './templates/index.mjs';
 import { logOutFunction } from './api/utilities/logOut.mjs';
 import { createPost } from './api/posts/create.mjs';
-import { updatePost } from './api/posts/update.mjs';
 import { searchBar } from './handlers/search.mjs';
 import { filterPosts } from './handlers/filter.mjs';
 import { runSpecificPost } from './templates/specificPost.mjs';
-import { getProfile } from './api/profiles/read.mjs';
 import { renderProfile } from './api/profiles/renderProfile.mjs';
 import { runProfilePosts } from './api/profiles/posts.mjs';
 import { updateProfileLink } from './api/utilities/updateProfileLink.mjs';
